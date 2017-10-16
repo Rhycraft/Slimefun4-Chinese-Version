48300fa4e40b663b35de97479f36bf695192a051
StarWishsama <1552409060@qq.com>
2017-10-16 18:31:25 +0800

    转换Unicode

--- src/me/mrCookieSlime/Slimefun/Setup/Files.java
+++ src/me/mrCookieSlime/Slimefun/Setup/Files.java
@@ -1,8 +1,3 @@
-// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
-// Jad home page: http://www.kpdus.com/jad.html
-// Decompiler options: packimports(3) fieldsfirst 
-// Source File Name:   Files.java
-
 package me.mrCookieSlime.Slimefun.Setup;
 
 import java.io.File;
@@ -27,21 +22,20 @@ public class Files
         {
             System.err.println("################################################");
             System.err.println("################################################");
-            System.err.println("############### = - \u9519\u8BEF - = ##################");
+            System.err.println("#################### = - 错误 - = ##################");
             System.err.println("################################################");
             System.err.println("################################################");
             System.err.println("      ");
-            System.err.println("Slimefun \u8B66\u544A:");
+            System.err.println("Slimefun 警告:");
             System.err.println("         ");
-            System.err.println("Slimefun \u68C0\u6D4B\u5230\u4F60\u7684\u6587\u4EF6");
-            System.err.println("\u5DF2\u8FC7\u671F\u6216\u4E0D\u5B58\u5728. \u6211\u4EEC\u521B\u5EFA\u4E86\u65B0\u7684\u6587\u4EF6");
-            System.err.println("\u4EE5\u786E\u4FDD Slimefun \u53EF\u4EE5\u5DE5\u4F5C. \u5982\u679C\u60A8");
-            System.err.println("\u66FE\u7ECF\u4F7F\u7528\u8FC7\u7C98\u6DB2\u79D1\u6280, \u4F60\u539F\u6765\u7684\u8BBE\u7F6E\u5C06\u4F1A\u4E22\u5931.");
-            System.err.println("\u4F46 Slimefun \u5DE5\u4F5C\u4E86 :)");
+            System.err.println("Slimefun 检测到你的文件已过期或不存在.);
+            System.err.println("我们创建了新的文件以确保 Slimefun 可以工作.");
+            System.err.println("如果您曾经使用过粘液科技, 你原来的设置将会丢失");
+            System.err.println("但 Slimefun 会正常工作 :)");
             System.err.println("        ");
             System.err.println("################################################");
             System.err.println("################################################");
-            System.err.println("############### = - \u9519\u8BEF - = ##################");
+            System.err.println("#################### = - 错误 - = ##################");
             System.err.println("################################################");
             System.err.println("################################################");
             delete(new File("plugins/Slimefun"));
